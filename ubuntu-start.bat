8ddde1713d745ff6be0c63fb524abd65e04381f7
KalyanR9 <rajputkalyan09@gmail.com>
2019-05-10 10:46:44 +0530

    hadoop script

---
+++ ubuntu-start.bat
@@ -0,0 +1,3 @@
+cd C:\Program Files\Oracle\VirtualBox
+VBoxManage startvm ubuntu --type headless
+REM VBoxManage controlvm ubuntu poweroff
---
+++ ubuntu-stop.bat
@@ -0,0 +1,3 @@
+cd C:\Program Files\Oracle\VirtualBox
+#VBoxManage startvm ubuntu --type headless
+VBoxManage controlvm ubuntu poweroff
